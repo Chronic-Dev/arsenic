03eccb672c6696a5b2e87b7c7762a07b98cd6a83
GreySyntax <greysyntax@gmail.com>
2011-01-15 22:04:27 +0000

    Started on Bundle class.

---
+++ Source/bundle.h
@@ -0,0 +1,38 @@
+/**
+ * GreenPois0n Arsenic
+ * Copyright (C) 2010 Chronic-Dev Team
+ * Copyright (C) 2010 GreySyntax
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+#include "common.h"
+
+namespace GP {
+    
+    class Bundle {
+        
+    public:
+        Bundle(const char* productType, const char* productBuild);
+        Bundle();
+        ~Bundle();
+        
+        static bool hasBundle(const char* productType, const char* productBuild);
+        
+    private:
+        const char* _type;
+        const char* _build;
+    
+    };
+}
