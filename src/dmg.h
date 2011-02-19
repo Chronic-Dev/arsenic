4d69ab973e48eaa6e9611c8fadfe7408374ad8ad
GreySyntax <greysyntax@gmail.com>
2011-02-19 12:36:25 +0000

    Added basic classes

--- src/dmg.h
+++ src/dmg.h
@@ -16,3 +16,15 @@
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
+
+#include "common.h"
+
+namespace GP {
+
+	class DMG {
+
+	public:
+		DMG();
+		~DMG();
+	};
+}
