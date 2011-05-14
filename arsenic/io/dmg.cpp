dac8b5d16d7e072fb215bbca3fae2a4435201e5d
GreySyntax <greysyntax@gmail.com>
2011-05-14 20:25:38 +0100

    Added the xpwn-hfs (dmg) wrapper Arsenic::IO::DMG.

---
+++ arsenic/io/dmg.cpp
@@ -0,0 +1,115 @@
+/**
+* GreenPois0n Arsenic
+* Copyright (C) 2010-2011 Chronic-Dev Team
+* Copyright (C) 2010-2011 GreySyntax
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
+#include "dmg.h"
+
+using namespace std;
+using namespace Arsenic::IO;
+
+namespace Arsenic {
+	namespace IO {
+		
+		// Constructor
+		DMG::DMG(AbstractFile *in, AbstractFile *out, const char* key, int partition) {
+			mIn = in;
+			mKey = key;
+			mOut = out;
+			mPartition = partition;
+		}
+		
+		// Destructor
+		DMG::~DMG() {
+			if (mIn) {
+				free(mIn);
+			}
+			if (mOut) {
+				free(mOut);
+			}
+			if (mKey) {
+				free(mKey);
+			}
+		}
+		
+		// Create a DMG
+		DMG* DMG::open(const char* source, const char* dest) {
+			return open(source, dest, NULL, -1);
+		}
+		
+		DMG* DMG::open(const char* source, const char* dest, int partition) {
+			return open(source, dest, NULL, partition);
+		}
+		
+		DMG* DMG::open(const char* source, const char* dest, const char* key) {
+			return open(source, dest, key, -1);
+		}
+		
+		DMG* DMG::open(const char* source, const char* dest, const char* key, int partition) {
+			AbstractFile* in, out;
+			bool hasKey = false;
+			
+			in = createAbstractFileFromFile(fopen(source, "rb"));
+			
+			if (in == NULL) {
+				LOG4CXX_ERROR(logger, "Failed to open file: " << source);
+				return NULL;
+			}
+			
+			out = createAbstractFileFromFile(fopen(dest, "wb"));
+			
+			if (out == NULL) {
+				in->close(in);
+				LOG4CXX_ERROR(logger, "Failed to open file: " << dest);
+				return NULL;
+			}
+			
+			// Encrypted using filevault
+			if (key != NULL) {
+				hasKey = true;
+				in = createAbstractFileFromFileVault(in, key);
+			}
+			
+			return (new DMG(in, out, key, partition))
+		}
+		
+		// DMG operations
+		void DMG::extract() {
+			extractDmg(mIn, mOut, mPartition);
+		}
+		
+		void DMG::build() {
+			build(SECTOR_SIZE);
+		}
+		
+		void DMG::build(int size) {
+			buildDmg(mIn, mOut, size);
+		}
+		
+		void DMG::resources() {
+			outResources(mIn, mOut);
+		}
+		
+		void DMG::toISO() {
+			convertToISO(mIn, mOut);
+		}
+		
+		void DMG::toDMG() {
+			convertToDMG(mIn, mOut);
+		}
+	}
+}
---
+++ arsenic/io/dmg.h
@@ -0,0 +1,63 @@
+/**
+* GreenPois0n Arsenic
+* Copyright (C) 2010-2011 Chronic-Dev Team
+* Copyright (C) 2010-2011 GreySyntax
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
+#ifndef ARSENIC_IO_DMG_
+#define ARSENIC_IO_MEMORYFILE_
+
+#include "../core/common.h"
+extern "C" {
+	#include <xpwn-hfs-1.0/abstractfile.h>
+	#include <xpwn-hfs-1.0/dmg.h>
+	#include <xpwn-hfs-1.0/filevault.h>
+}
+
+namespace Arsenic {
+	namespace IO {
+		
+		class DMG {
+		public:
+			// Destructor
+			~DMG();
+			
+			// Create a DMG
+			static DMG* open(const char* source, const char* dest);
+			static DMG* open(const char* source, const char* dest, int partition);
+			static DMG* open(const char* source, const char* dest, const char* key);
+			static DMG* open(const char* source, const char* dest, const char* key, int partition);
+			
+			// DMG operations
+			void extract();
+			void build();
+			void build(int size);
+			void resources();
+			void toISO();
+			void toDMG();
+			
+		private:
+			// Constructor
+			DMG(AbstractFile* in, AbstractFile* out, const char* key, int partition);
+			
+			int mPartition;
+			const char* mKey;
+			AbstractFile* mIn;
+			AbstractFile* mOut;
+		};
+	}
+}
+#endif /* ARSENIC_IO_DMG_ */
